d86bade94fca8951b2588a248d774772705c3488
Tayayi Muyard <muyardmayam.t@gmail.com>
2026-09-19 15:07:17 +0200

    feat: add mobile navbar blur overlay and highlight the active nav link

--- js/controllers/navbar_controller.js
+++ js/controllers/navbar_controller.js
@@ -1,21 +1,25 @@
 import { Controller } from "https://unpkg.com/@hotwired/stimulus@3.2.2/dist/stimulus.js";
 
 export default class extends Controller {
-  static targets = ["toggler"];
+  static targets = ["nav", "toggler", "backdrop", "collapse"];
 
   connect() {
     this.onScroll();
   }
 
   onScroll() {
-    this.element.classList.toggle("is-scrolled", window.scrollY > 10);
+    this.navTarget.classList.toggle("is-scrolled", window.scrollY > 10);
   }
 
   onShown() {
     this.togglerTarget.classList.add("is-open");
+    this.backdropTarget.style.height = `${this.collapseTarget.offsetHeight}px`;
+    this.backdropTarget.classList.add("is-visible");
   }
 
   onHidden() {
     this.togglerTarget.classList.remove("is-open");
+    this.backdropTarget.classList.remove("is-visible");
+    this.backdropTarget.style.height = "0";
   }
 }
