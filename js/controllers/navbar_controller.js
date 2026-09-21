e852391e958ad5d2f641a385201a023ad052b889
Tayayi Muyard <muyardmayam.t@gmail.com>
2026-09-21 14:19:29 +0200

    fix: extend mobile navbar backdrop to cover the full viewport

--- js/controllers/navbar_controller.js
+++ js/controllers/navbar_controller.js
@@ -5,21 +5,29 @@ export default class extends Controller {
 
   connect() {
     this.onScroll();
+    this.resizeObserver = new ResizeObserver(() => this.syncHeight());
+    this.resizeObserver.observe(this.collapseTarget);
+  }
+
+  disconnect() {
+    this.resizeObserver?.disconnect();
   }
 
   onScroll() {
     this.navTarget.classList.toggle("is-scrolled", window.scrollY > 10);
   }
 
-  onShown() {
+  syncHeight() {
+    this.backdropTarget.style.height = `${this.navTarget.offsetHeight + this.collapseTarget.offsetHeight}px`;
+  }
+
+  onShow() {
     this.togglerTarget.classList.add("is-open");
-    this.backdropTarget.style.height = `${this.collapseTarget.offsetHeight}px`;
     this.backdropTarget.classList.add("is-visible");
   }
 
-  onHidden() {
+  onHide() {
     this.togglerTarget.classList.remove("is-open");
     this.backdropTarget.classList.remove("is-visible");
-    this.backdropTarget.style.height = "0";
   }
 }
